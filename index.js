2b5486d9a8e1ffaf5bbe5da615016721527e1c78
Matthieu Achard <matthieu__@hotmail.fr>
2017-01-26 14:40:16 +0700

    expose relative path

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
+var path = require('path');
 module.exports = function() {
-  return "try {global.process.dlopen(module, " + JSON.stringify(this.resourcePath) + "); } catch(e) {" +
-    "throw new Error('Cannot open ' + " + JSON.stringify(this.resourcePath) + " + ': ' + e);}";
+  return "try {global.process.dlopen(module, " + JSON.stringify('./' + path.relative(this.options.context, this.resourcePath)) + "); } catch(e) {" +
+    "throw new Error('Cannot open ' + " + JSON.stringify('./' + path.relative(this.options.context, this.resourcePath)) + " + ': ' + e);}";
 }
